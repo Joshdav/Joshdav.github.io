0517a72ccef2b90d8bd24a6980bf4d25ce301c05
Joshdav <joshdav42@gmail.com>
2016-02-21 19:50:09 -0800

    Fixed mobile, added more icons
    Fixed scaling on mobile and smaller screens in the HTML with the xs, md,
    and lg tags. Added rings and most of the void armor.

--- js/app.js
+++ js/app.js
@@ -44,11 +44,59 @@ window.onload = function() {
       if (cookiearr[i].trim() == "dragonwarhammerclick=true") {
         $('.dragonwarhammer').css('opacity', '1');
       }
+      if (cookiearr[i].trim() == "dragondefenderclick=true") {
+        $('.dragondefender').css('opacity', '1');
+      }
       if (cookiearr[i].trim() == "dragonpickaxeclick=true") {
         $('.dragonpickaxe').css('opacity', '1');
       }
       if (cookiearr[i].trim() == "dragonaxeclick=true") {
         $('.dragonaxe').css('opacity', '1');
+      }
+      if (cookiearr[i].trim() == "berserkerringclick=true") {
+        $('.berserkerring').css('opacity', '1');
+      }
+      if (cookiearr[i].trim() == "seersringclick=true") {
+        $('.seersring').css('opacity', '1');
+      }
+      if (cookiearr[i].trim() == "archersringclick=true") {
+        $('.archersring').css('opacity', '1');
+      }
+      if (cookiearr[i].trim() == "warriorringclick=true") {
+        $('.warriorring').css('opacity', '1');
+      }
+      if (cookiearr[i].trim() == "seercullclick=true") {
+        $('.seercull').css('opacity', '1');
+      }
+      if (cookiearr[i].trim() == "mudbattlestaffclick=true") {
+        $('.mudbattlestaff').css('opacity', '1');
+      }
+      if (cookiearr[i].trim() == "treasonousringclick=true") {
+        $('.treasonousring').css('opacity', '1');
+      }
+      if (cookiearr[i].trim() == "tyrannicalringclick=true") {
+        $('.tyrannicalring').css('opacity', '1');
+      }
+      if (cookiearr[i].trim() == "ringofthegodsclick=true") {
+        $('.ringofthegods').css('opacity', '1');
+      }
+      if (cookiearr[i].trim() == "voidrangeclick=true") {
+        $('.voidrange').css('opacity', '1');
+      }
+      if (cookiearr[i].trim() == "voidmeleeclick=true") {
+        $('.voidmelee').css('opacity', '1');
+      }
+      if (cookiearr[i].trim() == "voidmageclick=true") {
+        $('.voidmage').css('opacity', '1');
+      }
+      if (cookiearr[i].trim() == "voidtopclick=true") {
+        $('.voidtop').css('opacity', '1');
+      }
+      if (cookiearr[i].trim() == "voidelitetopclick=true") {
+        $('.voidelitetop').css('opacity', '1');
+      }
+      if (cookiearr[i].trim() == "voidmaceclick=true") {
+        $('.voidmace').css('opacity', '1');
       }
    	}
 }
@@ -202,6 +250,17 @@ var main = function() {
         }
   });
 
+  $('.dragondefender').click(function(){
+    if ($(this).css('opacity') == '1') {
+          $(this).animate({'opacity':0.4}, 175);
+          document.cookie = "dragondefenderclick=false;" + expires + ";";
+    }
+        else {
+          $(this).animate({'opacity':1}, 175);
+          document.cookie = "dragondefenderclick=true;" + expires + ";";
+        }
+  });
+
   $('.dragonpickaxe').click(function(){
     if ($(this).css('opacity') == '1') {
           $(this).animate({'opacity':0.4}, 175);
@@ -223,5 +282,171 @@ var main = function() {
           document.cookie = "dragonaxeclick=true;" + expires + ";";
         }
   });
+
+  $('.berserkerring').click(function(){
+    if ($(this).css('opacity') == '1') {
+          $(this).animate({'opacity':0.4}, 175);
+          document.cookie = "berserkerringclick=false;" + expires + ";";
+    }
+        else {
+          $(this).animate({'opacity':1}, 175);
+          document.cookie = "berserkerringclick=true;" + expires + ";";
+        }
+  });
+
+  $('.seersring').click(function(){
+    if ($(this).css('opacity') == '1') {
+          $(this).animate({'opacity':0.4}, 175);
+          document.cookie = "seersringclick=false;" + expires + ";";
+    }
+        else {
+          $(this).animate({'opacity':1}, 175);
+          document.cookie = "seersringclick=true;" + expires + ";";
+        }
+  });
+
+  $('.archersring').click(function(){
+    if ($(this).css('opacity') == '1') {
+          $(this).animate({'opacity':0.4}, 175);
+          document.cookie = "archersringclick=false;" + expires + ";";
+    }
+        else {
+          $(this).animate({'opacity':1}, 175);
+          document.cookie = "archersringclick=true;" + expires + ";";
+        }
+  });
+
+  $('.warriorring').click(function(){
+    if ($(this).css('opacity') == '1') {
+          $(this).animate({'opacity':0.4}, 175);
+          document.cookie = "warriorringclick=false;" + expires + ";";
+    }
+        else {
+          $(this).animate({'opacity':1}, 175);
+          document.cookie = "warriorringclick=true;" + expires + ";";
+        }
+  });
+
+  $('.seercull').click(function(){
+    if ($(this).css('opacity') == '1') {
+          $(this).animate({'opacity':0.4}, 175);
+          document.cookie = "seercullclick=false;" + expires + ";";
+    }
+        else {
+          $(this).animate({'opacity':1}, 175);
+          document.cookie = "seercullclick=true;" + expires + ";";
+        }
+  });
+
+  $('.mudbattlestaff').click(function(){
+    if ($(this).css('opacity') == '1') {
+          $(this).animate({'opacity':0.4}, 175);
+          document.cookie = "mudbattlestaffclick=false;" + expires + ";";
+    }
+        else {
+          $(this).animate({'opacity':1}, 175);
+          document.cookie = "mudbattlestaffclick=true;" + expires + ";";
+        }
+  });
+
+  $('.treasonousring').click(function(){
+    if ($(this).css('opacity') == '1') {
+          $(this).animate({'opacity':0.4}, 175);
+          document.cookie = "treasonousringclick=false;" + expires + ";";
+    }
+        else {
+          $(this).animate({'opacity':1}, 175);
+          document.cookie = "treasonousringclick=true;" + expires + ";";
+        }
+  });
+
+  $('.tyrannicalring').click(function(){
+    if ($(this).css('opacity') == '1') {
+          $(this).animate({'opacity':0.4}, 175);
+          document.cookie = "tyrannicalringclick=false;" + expires + ";";
+    }
+        else {
+          $(this).animate({'opacity':1}, 175);
+          document.cookie = "tyrannicalringclick=true;" + expires + ";";
+        }
+  });
+
+  $('.ringofthegods').click(function(){
+    if ($(this).css('opacity') == '1') {
+          $(this).animate({'opacity':0.4}, 175);
+          document.cookie = "ringofthegodsclick=false;" + expires + ";";
+    }
+        else {
+          $(this).animate({'opacity':1}, 175);
+          document.cookie = "ringofthegodsclick=true;" + expires + ";";
+        }
+  });
+
+  $('.voidrange').click(function(){
+    if ($(this).css('opacity') == '1') {
+          $(this).animate({'opacity':0.4}, 175);
+          document.cookie = "voidrangeclick=false;" + expires + ";";
+    }
+        else {
+          $(this).animate({'opacity':1}, 175);
+          document.cookie = "voidrangeclick=true;" + expires + ";";
+        }
+  });
+
+  $('.voidmelee').click(function(){
+    if ($(this).css('opacity') == '1') {
+          $(this).animate({'opacity':0.4}, 175);
+          document.cookie = "voidmeleeclick=false;" + expires + ";";
+    }
+        else {
+          $(this).animate({'opacity':1}, 175);
+          document.cookie = "voidmeleeclick=true;" + expires + ";";
+        }
+  });
+
+  $('.voidmage').click(function(){
+    if ($(this).css('opacity') == '1') {
+          $(this).animate({'opacity':0.4}, 175);
+          document.cookie = "voidmageclick=false;" + expires + ";";
+    }
+        else {
+          $(this).animate({'opacity':1}, 175);
+          document.cookie = "voidmageclick=true;" + expires + ";";
+        }
+  });
+
+  $('.voidtop').click(function(){
+    if ($(this).css('opacity') == '1') {
+          $(this).animate({'opacity':0.4}, 175);
+          document.cookie = "voidtopclick=false;" + expires + ";";
+    }
+        else {
+          $(this).animate({'opacity':1}, 175);
+          document.cookie = "voidtopclick=true;" + expires + ";";
+        }
+  });
+
+  $('.voidelitetop').click(function(){
+    if ($(this).css('opacity') == '1') {
+          $(this).animate({'opacity':0.4}, 175);
+          document.cookie = "voidelitetopclick=false;" + expires + ";";
+    }
+        else {
+          $(this).animate({'opacity':1}, 175);
+          document.cookie = "voidelitetopclick=true;" + expires + ";";
+        }
+  });
+
+  $('.voidmace').click(function(){
+    if ($(this).css('opacity') == '1') {
+          $(this).animate({'opacity':0.4}, 175);
+          document.cookie = "voidmaceclick=false;" + expires + ";";
+    }
+        else {
+          $(this).animate({'opacity':1}, 175);
+          document.cookie = "voidmaceclick=true;" + expires + ";";
+        }
+  });
+
 }
 $(document).ready(main);
